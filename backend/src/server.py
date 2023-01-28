ab59cd0dcf0e18777bac9c35c65b312343e8bc83
Nikola Milekic <nikola.milekic5@gmail.com>
2023-01-28 17:08:50 -0500

    Add listing endpoint

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,15 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify
 from dotenv import load_dotenv
 import os
+import cohere
+import conversant
+import pathlib
 
 load_dotenv()
 COHERE_API_KEY = os.environ.get('APIKEY')
 
 app = Flask(__name__)
+personaDir = str(pathlib.Path().resolve())+"/src/personas"
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    co = cohere.Client(COHERE_API_KEY)
+    bot = conversant.PromptChatbot.from_persona("friend", client=co, persona_dir=personaDir)
+    return bot.reply("How old are you?")
+
+@app.route('/list-personas')
+def listPersonas():
+    personaNames = os.listdir(personaDir)
+    return jsonify(personaNames)
+    
+
 
 if __name__ == '__main__':
     app.run(host="localhost", port=3001, debug=True)
