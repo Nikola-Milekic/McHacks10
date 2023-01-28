b46230ade090d6fd00060ddc277b593c3c90194f
Eric Xie <66566975+ericcxie@users.noreply.github.com>
2023-01-28 18:39:28 -0500

    Implemented text loading animation

--- frontend/src/pages/BuddyChat.tsx
+++ frontend/src/pages/BuddyChat.tsx
@@ -1,9 +1,13 @@
-function BuddyChat(){
-    return(
-        <div>
-            <h1>Buddy Chat</h1>
-            <p>Some content</p>
-        </div>
-    )
+function BuddyChat() {
+  return (
+    <section>
+      <div className="p-8 lg:py-5">
+        <p className="text-gray-500 text-lg lg:text-xl">Step 2:</p>
+        <h1 className="text-2xl py-2 lg:text-3xl text-gray-700 font-semibold">
+          👋 Say <span className="text-primary">Hi</span> to Friend!
+        </h1>
+      </div>
+    </section>
+  );
 }
 export default BuddyChat;
--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -2,21 +2,28 @@ import { Button, Grid } from "@mui/material";
 import "../App.css";
 import main from "../landing.svg";
 import { Navigate } from "react-router-dom";
+import { useEffect } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 function Landing() {
+  useEffect(() => {
+    Aos.init({ duration: 800 });
+  }, []);
+
   return (
-    <section>
+    <section data-aos="fade-up" data-aos-once data-aos-delay="500">
       <div className="flex flex-col h-screen items-center justify-center">
         <div className="mb-12 space-y-3">
           <h1 className="text-5xl font-bold text-gray-600">
-            Build-a-<span className="text-[#6C63FF]">Buddy</span>
+            Build-a-<span className="text-primary">Buddy</span>
           </h1>
           <p className="text-lg text-center text-gray-600 italic font-semibold">
-            Configure your dream <span className="text-[#6C63FF]">friend</span>.
+            Configure your dream <span className="text-primary">friend</span>.
           </p>
         </div>
         <img src={main} width={300} alt="SVG" />
-        <button className="py-3 px-12 bg-[#6C63FF] text-white font-semibold rounded-2xl mt-16 hover:shadow-lg hover:bg-[#4e48be]">
+        <button className="py-3 px-12 bg-primary text-white font-semibold rounded-2xl mt-16 hover:shadow-lg hover:bg-[#4e48be]">
           Build now
         </button>
       </div>
