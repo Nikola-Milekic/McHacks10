e1a9258281ac56c3a72b41a0207d29240c60fa0d
Eric Xie <66566975+ericcxie@users.noreply.github.com>
2023-01-28 17:53:19 -0500

    Added Landing page

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,9 +1,26 @@
-function Landing(){
-    return (
-        <div>
-        <h1>Landing</h1>
-        <p>Some content</p>
+import { Button, Grid } from "@mui/material";
+import "../App.css";
+import main from "../landing.svg";
+import { Navigate } from "react-router-dom";
+
+function Landing() {
+  return (
+    <section>
+      <div className="flex flex-col h-screen items-center justify-center">
+        <div className="mb-12 space-y-3">
+          <h1 className="text-5xl font-bold text-gray-600">
+            Build-a-<span className="text-[#6C63FF]">Buddy</span>
+          </h1>
+          <p className="text-lg text-center text-gray-600 italic font-semibold">
+            Configure your dream <span className="text-[#6C63FF]">friend</span>.
+          </p>
         </div>
-    )
+        <img src={main} width={300} alt="SVG" />
+        <button className="py-3 px-12 bg-[#6C63FF] text-white font-semibold rounded-2xl mt-16 hover:shadow-lg hover:bg-[#4e48be]">
+          Build now
+        </button>
+      </div>
+    </section>
+  );
 }
 export default Landing;
