c522633dec650a0bff3ed243baee2503b532459c
James <jamesjglee73@gmail.com>
2023-01-28 21:43:16 -0500

    Fix merge

--- frontend/src/pages/Questions.tsx
+++ frontend/src/pages/Questions.tsx
@@ -1,46 +1,4 @@
-<<<<<<< HEAD
-import * as React from "react";
-import { styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
-import PersonalSettingsImg from "../assets/images/PersonalSettingsImg";
-import BuddiesImg from "../assets/images/BuddiesImg";
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
-
-function Questions() {
-  return (
-    <>
-      <Box sx={{ padding: 10 }}>
-        <h1>Build your friend</h1>
-        <Box sx={{ flexGrow: 1 }}>
-          <Grid
-            container
-            spacing={2}
-            sx={{ borderRadius: "22px", alignItems: "center" }}
-          >
-            <Grid item xs={6}>
-              <Grid container spacing={1}>
-                <Item>
-                  <h2>What is your name?</h2>
-                </Item>
-              </Grid>
-            </Grid>
-            <Grid item xs={6}></Grid>
-          </Grid>
-        </Box>
-        <BuddiesImg></BuddiesImg>
-      </Box>
-    </>
-  );
-=======
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
