4be2b89528a4ec0c666e4b6feba82e3cbe437b56
Nikola Milekic <nikola.milekic5@gmail.com>
2023-01-28 21:24:35 -0500

    Add post endpoints

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask, jsonify, request
 from dotenv import load_dotenv
 from pathlib import Path
@@ -12,36 +13,57 @@ COHERE_API_KEY = os.environ.get('APIKEY')
 
 app = Flask(__name__)
 personaDir = str(pathlib.Path().resolve())+"/src/personas"
+srcDir = str(pathlib.Path().resolve())+"/src"
+co = cohere.Client(COHERE_API_KEY)
 
 @app.route('/')
 def hello():
-    co = cohere.Client(COHERE_API_KEY)
     bot = conversant.PromptChatbot.from_persona("friend", client=co, persona_dir=personaDir)
-    return bot.reply("How old are you?")
+    bot.reply("What school do you go to?")
+    return (bot.chat_history)
 
 @app.route('/list-personas')
 def listPersonas():
     personaNames = os.listdir(personaDir)
     return jsonify(personaNames)
 
-@app.route('/add-persona', methods=['GET', 'POST'])
+@app.route('/add-persona', methods=['POST'])
 def add_persona():
     content = request.json
     name = content['name'].lower()
     examples = content['examples']
 
-    with open('personas/template.json') as f:
+    with open(f'{srcDir}/template.json') as f:
         template = json.load(f)
-    template['chat_prompt_config']['examples'] = examples
+    template['chat_prompt_config']['examples'] = [examples,[]]
 
-    os.mkdir(f'personas/{name}')
+    if not os.path.exists(f'{personaDir}/{name}'):
+        os.mkdir(f'{personaDir}/{name}')
     out = json.dumps(template, indent=4)
-    with open(f'personas/{name}/config.json', 'w+') as f:
+    with open(f'{personaDir}/{name}/config.json', 'w') as f:
         f.write(out)
 
     return "all good bro"
     
-
+@app.route('/chat/<name>', methods = ['POST', 'GET'])
+def chat_with_friend(name):
+    name = name.lower()
+    if(request.method == 'POST'):
+        req = request.json
+        prompt = req['prompt']
+        bot = conversant.PromptChatbot.from_persona(name, client=co, persona_dir=personaDir)
+        bot.reply(prompt)
+        with open(f'{personaDir}/{name}/config.json') as f:
+            persona = json.load(f)
+        persona['chat_prompt_config']['examples'][-1].append(bot.chat_history[0])
+        out = json.dumps(persona, indent=4)
+        with open(f'{personaDir}/{name}/config.json', "w") as f:
+            f.write(out)
+        return jsonify(persona['chat_prompt_config']['examples'][-1])
+    elif(request.method == 'GET'):
+        with open(f'{personaDir}/{name}/config.json') as f:
+            persona = json.load(f)
+        return jsonify(persona['chat_prompt_config']['examples'][-1])
 
 if __name__ == '__main__':
     app.run(host="localhost", port=3001, debug=True)
